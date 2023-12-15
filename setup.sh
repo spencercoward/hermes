9bb3270b1cccc5f598e11b06ed476066f7ccf5e5
james j coward <jcoward59@gmail.com>
2023-12-15 20:22:43 +0000

    dad

--- setup.sh
+++ setup.sh
@@ -6,63 +6,45 @@ echo // written by scoward
 echo ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 BUILD_TYPE=Debug
-HERMES_CMAKE_CONFIGURE_COMMAND="cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=$BUILD_TYPE"
-HERMES_CMAKE_BUILD_COMMAND="cmake --build . -j 24"
-HERMES_CMAKE_INSTALL_COMMAND="cmake --install . --prefix "../../../install""
+HERMES_CMAKE_CONFIGURE_COMMAND="cmake -S . -B hermes-build -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=$PWD/external/install" 
+HERMES_CMAKE_BUILD_COMMAND="cmake --build hermes-build -j 24"
+HERMES_CMAKE_INSTALL_COMMAND="cmake --install hermes-build "
 
 # setup the location for cloning external libs
-mkdir external
-pushd external
-
-# create a location to clone things to
-mkdir sources
-
-# create a location to install the libs to so we dont have to keep building them after initial setup
-mkdir install
+mkdir -p external/sources
+mkdir -p external/install
 
 # start cloning things
-pushd sources
+pushd external/sources
 
 git clone --recursive --depth 1 https://github.com/pocoproject/poco.git
-
 git clone --recursive --depth 1 https://github.com/zeromq/libzmq.git
-
 git clone --recursive --depth 1 https://github.com/google/flatbuffers.git
 
 # build the libs
 pushd poco
-mkdir hermes-build
-pushd hermes-build
 
 $HERMES_CMAKE_CONFIGURE_COMMAND
 $HERMES_CMAKE_BUILD_COMMAND
 $HERMES_CMAKE_INSTALL_COMMAND
 
-popd
 popd
 
 pushd libzmq
-mkdir hermes-build
-pushd hermes-build
 
 $HERMES_CMAKE_CONFIGURE_COMMAND
 $HERMES_CMAKE_BUILD_COMMAND
 $HERMES_CMAKE_INSTALL_COMMAND
 
-popd
 popd
 
 pushd flatbuffers
-mkdir hermes-build
-pushd hermes-build
 
 $HERMES_CMAKE_CONFIGURE_COMMAND
 $HERMES_CMAKE_BUILD_COMMAND
 $HERMES_CMAKE_INSTALL_COMMAND
 
 popd
-popd
-
 
 echo Done setting up
 echo 
