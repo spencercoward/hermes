91a15c3cfb22e5fa429193fa31dea88f4035a92b
Spencer J. Coward <spencerc211@gmail.com>
2023-12-15 19:52:29 -0500

    working on implementing poco app

--- include/hermes/hermes.h
+++ include/hermes/hermes.h
@@ -1,18 +1,39 @@
 #ifndef HERMES_H
 #define HERMES_H
-
-#include <cstdint>
+#include <iostream>
+#include <sstream>
+#include <memory>
+#include "Poco/Util/Application.h"
 
 namespace hermes
 {
 
-// struct Pulse
-// {
-// 	std::uint64_t m_toa;
-// 	std::uint64_t m_frequency;
-// 	std::uint64_t m_amplitude;
-// 	std::uint64_t m_pulseWidth;
-// };
+class Hermes : public Poco::Util::Application
+{
+public:
+	Hermes();
 
+protected:
+	void initialize(Application &self);
+
+	void uninitialize();
+
+	void reinitialize(Application &self);
+
+	void defineOptions(Poco::Util::OptionSet &options);
+
+	void handleHelp(const std::string &name, const std::string &value);
+
+	void handleConfig(const std::string &name, const std::string &value);
+
+	void displayHelp();
+
+	int main(const ArgVec &args);
+
+private:
+	bool _helpRequested;
+	std::string _roleString;
+	std::unique_ptr<Poco::Runnable> _runnable;
+};
 } // namespace hermes
 #endif // HERMES_H
--- src/hermes.cpp
+++ src/hermes.cpp
@@ -1,23 +1,125 @@
 // // written by scoward
 
-#include <Poco/Stopwatch.h>
 #include <zmq.hpp>
+
+#include <Poco/Util/Option.h>
+#include <Poco/Util/OptionSet.h>
+#include <Poco/Util/HelpFormatter.h>
+#include <Poco/AutoPtr.h>
+#include <Poco/ConsoleChannel.h>
+
 #include "hermes/hermes.h"
 #include "hermes/Pulse_generated.h"
 
-int main()
+namespace hermes
 {
-	flatbuffers::FlatBufferBuilder builder(1024);
 
-	auto pulse = hermes::CreatePulse(builder, 0, 0, 0, 0);
-
-	Poco::Stopwatch sw;
-
-	sw.start();
-	for (int i(0); i < 0xFFFF; ++i)
-	{
-	}
-	sw.stop();
-	return 0;
+Hermes::Hermes()
+	: _helpRequested(false)
+{
 }
+
+void Hermes::initialize(Application &self)
+{
+	Application::initialize(self);
+	// add your own initialization code here
+	Poco::AutoPtr<Poco::ColorConsoleChannel> pCCC =
+		new Poco::ColorConsoleChannel;
+	// pCCC->setProperty("traceColor", "");
+	// pCCC->setProperty("debugColor", "");
+	// pCCC->setProperty("informationColor", "");
+	// pCCC->setProperty("noticeColor", "");
+	// pCCC->setProperty("warningColor", "");
+	// pCCC->setProperty("errorColor", "");
+	// pCCC->setProperty("criticalColor", "");
+	// pCCC->setProperty("fatalColor", "");
+	logger().setChannel("", pCCC);
+}
+
+void Hermes::uninitialize()
+{
+	// add your own uninitialization code here
+	Application::uninitialize();
+}
+
+void Hermes::reinitialize(Application &self)
+{
+	Application::reinitialize(self);
+	// add your own reinitialization code here
+}
+
+void Hermes::defineOptions(Poco::Util::OptionSet &options)
+{
+	Application::defineOptions(options);
+
+	options.addOption(
+		Poco::Util::Option(
+			"help", "h",
+			"display help information on command line arguments")
+			.required(false)
+			.repeatable(false)
+			.callback(Poco::Util::OptionCallback<Hermes>(
+				this, &Hermes::handleHelp)));
+
+	options.addOption(
+		Poco::Util::Option("role", "r", "role to use [pub/sub]")
+			.required(true)
+			.repeatable(false)
+			.callback(Poco::Util::OptionCallback<Hermes>(
+				this, &Hermes::handleConfig)));
+
+	options.addOption(
+		Poco::Util::Option("ipaddr", "i", "ip addr to send/receive on")
+			.required(false)
+			.repeatable(false));
+}
+
+void Hermes::handleHelp(const std::string &name, const std::string &value)
+{
+	_helpRequested = true;
+	displayHelp();
+	stopOptionsProcessing();
+}
+
+void Hermes::handleConfig(const std::string &name, const std::string &value)
+{
+	_roleString = value;
+}
+
+void Hermes::displayHelp()
+{
+	Poco::Util::HelpFormatter helpFormatter(options());
+	helpFormatter.setCommand(commandName());
+	helpFormatter.setUsage("OPTIONS");
+	helpFormatter.setHeader(
+		"Example Pub Sub application using FlatBuffers and ZeroMQ");
+	helpFormatter.format(std::cout);
+}
+
+int Hermes::main(const ArgVec &args)
+{
+	if (!_helpRequested)
+	{
+		logger().information("Command line:");
+		std::ostringstream ostr;
+		for (ArgVec::const_iterator it = argv().begin();
+		     it != argv().end(); ++it)
+		{
+			ostr << *it << ' ';
+		}
+		logger().information(ostr.str());
+		logger().information("Arguments to main():");
+		for (ArgVec::const_iterator it = args.begin(); it != args.end();
+		     ++it)
+		{
+			logger().information(*it);
+		}
+	}
+	return Application::EXIT_OK;
+}
+
+} // namespace hermes
+
+POCO_APP_MAIN(hermes::Hermes)
+
 // // written by scoward
